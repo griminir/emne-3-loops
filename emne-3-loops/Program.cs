e368da83bb68bc919e522ed3ac6d7ec90ba7ba8b
griminir <viktor.elliot.king@gmail.com>
2024-05-01 19:30:03 +0200

    Add project files.

---
+++ emne-3-loops/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("LOOOPS");
+
+//oppgave 1
+void terjeForLoop()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Terje er kul");
+    }
+}
+terjeForLoop();
+
+//oppgave 2
+string noenOrd = "noen ord for en for each loop";
+
+void testForEachLoop(string words)
+{
+    foreach (char letter in words)
+    {
+        Console.WriteLine(letter);
+    }
+}
+testForEachLoop(noenOrd);
+
+//oppgave 3
+bool countTo10 = true;
+int i = 1;
+
+void roundCount()
+{
+    while (countTo10)
+    {
+        if (i < 10)
+        {
+            Console.WriteLine($"round number: {i}");
+            i++;
+        }
+        else
+        {
+            countTo10 = false;
+        }
+    }
+}
+roundCount();
